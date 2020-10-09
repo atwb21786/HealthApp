d2657b50f6bf171d4eb9e65f024f7dd5c1aa0ad3
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-10-08 18:18:31 -0700

    structure to the landing page landscape, wireframing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Health and Wellness</h1>
+        </header>
+        <main className="App-content">
+          <div className="App-about">
+            <section>
+              <h3>Fitness for the body and mind</h3>
+              <p>Staying healthy is hard. Staying healthy in 2020 during a global pandemic? Really hard.<br/>
+              The first step in getting healthy is knowing what that means. In times where misinformation is rife<br/>
+              it would be great to have a single place to find reliable health information, a place<br/>
+              that has great articles to read and podcasts that provided the best scientific information out there.<br/>
+               
+              </p>
+            </section>
+          </div>
+          <div className="App-resources">
+            <section className="App-articles">
+              <h3>Articles</h3>
+            </section>
+            <section className="App-podcasts">
+              <h3>Podcasts</h3>
+          </section>
+          </div>
+
+        </main>
+        <footer>
+
+        </footer>
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
