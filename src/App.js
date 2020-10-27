e1ebbc4c166d3a4b90296b6f19efb25f6f834499
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-10-27 09:01:22 -0700

    landing page images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Backpacker from './images/backpacker.jpg';
+import PeterAttia from './images/Peter-Attia-Hero.jpg';
+import TimFerriss from './images/TimFerriss.jpg';
+import RhondaPatrick from './images/RhondaPatrick.png';
+import PeterAttiaPodcast from './images/peterattiapodcastimage.png';
+import TimFerrissPodcast from './images/TimFerrissPodcast.jpg';
+import FoundMyFitness from './images/FoundMyFitness.webp';
 import './App.css';
 
 
@@ -34,16 +40,52 @@ class App extends React.Component {
           <div className="App-articles">
             <section>
               <h3>Articles</h3>
+                <section className="App-article-items">
+                  <div className="App-article-1">Article 1</div>
+                  <div className="App-article-2">Article 2</div>
+                  <div className="App-article-3">Article 3</div>
+                </section>
+                
             </section>
           </div>
           <div className="App-websites">
             <section>
               <h3>Websites</h3>
+                <section className="App-website-items">
+                  <div className="App-website-1">
+                    <img src={PeterAttia} alt="Peter_Attia_Hero"></img>
+                    <h4>Peter Attia M.D.</h4>
+                  </div>
+                  <div className="App-website-2">
+                    <img src={TimFerriss} alt="Tim_Ferriss"></img>
+                    <h4>Tim Ferriss</h4>
+                  </div>
+                  <div className="App-website-3">
+                    <img src={RhondaPatrick} alt="Rhonda_Patrick"></img>
+                    <h4>Found My Fitness</h4>
+                  </div>
+                </section>
+                
             </section>
           </div>
           <div className="App-podcasts">
             <section >
-                <h3>Podcasts</h3>
+              <h3>Podcasts</h3>
+              <section className="App-podcast-items">
+                <div className="App-podcast-1">
+                  <img src={PeterAttiaPodcast} alt="Peter_Attia_Podcast"></img>
+                  <h4>The Drive with Peter Attia</h4>
+                </div>
+                <div className="App-podcast-2">
+                  <img src={TimFerrissPodcast} alt="Tim_Ferriss_Podcast"></img>
+                  <h4>The Tim Ferriss Show</h4>
+                </div>
+                <div className="App-podcast-3">
+                  <img src={FoundMyFitness} alt="Found_My_Fitness"></img>
+                  <h4>Found My Fitness with Rhonda Patrick</h4>
+                </div>
+              </section>
+                
             </section>
           </div>
           <div className="App-fitness">
